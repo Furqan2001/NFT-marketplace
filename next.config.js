4e4b325412bbdfcabaaf078084d3cac83cf49b4a
Furqan Abid <furqan.abid01@gmail.com>
2022-03-17 15:32:35 +0500

    Added image loader

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   images: {
-    domains: ["ipfs.infura.io"]
+    domains: ["ipfs.infura.io"],
+    loader: (width, src, quality) =>
+      `/preprocessed-images/${filename}-${width}.jpg`
   },
   reactStrictMode: true
 };
